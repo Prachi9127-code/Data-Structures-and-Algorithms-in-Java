fd9a096968e3a215759540a0328d0593bba6b20a
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2022-05-25 21:29:03 +0530

    Add files via upload

---
+++ GenericTree.java
@@ -0,0 +1,22 @@
+package Tree;
+
+public class GenericTree {
+
+	public static boolean checkIfContainsX(TreeNode<Integer> root, int x){
+
+		if(root == null){
+            return false;
+        }
+        if(root.data == x){
+            return true;
+        }
+        for(TreeNode<Integer> child : root.children){
+            if(checkIfContainsX(child, x)){
+                return true;
+            }
+        }
+        return false;
+
+	}
+	
+}
